72a4a72f7945802302726eeed18030221360b050
Moisés J. Bonilla Caraballo <moisesjbc@gmail.com>
2019-01-22 22:54:01 +0000

    Added export bash script

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # godot-game-boilerplate
+
+This is a simple Godot engine project including dummy menus (main menú, credits and how to play) and a dummy main scene.
+
+This project also includes a simple `export.sh` script (Linux only) for exporting project to multiple platforms (Windows, Linux and HTML).
+
+## Exporting (Linux only)
+
+Run the export script. Packages will be generated in `export/` and `export/zip` directories.
+
+        bash export.sh <game-name> <version>
+
+For example:
+
+        bash export.sh awesome-game v1.0
---
+++ export.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+GODOT_EXECUTABLE=/opt/godot-engine/Godot_v3.0.6-stable_x11.64
+
+EXPORT_DIRPATH=`pwd`/export
+ZIP_DIRPATH=`pwd`"/export/zip"
+
+name=$1
+version=$2
+
+if [ -z "$name" ]; then
+    echo "ERROR: Expected a name"
+    exit 1
+fi
+
+if [ -z "$version" ]; then
+    echo "ERROR: Expected a version"
+    exit 1
+fi
+
+rm -r "$ZIP_DIRPATH"
+mkdir -p "$ZIP_DIRPATH"
+
+function export_game {
+    export_preset=$1
+    export_dirname=$2
+    executable_filename=$3
+
+    export_dirpath="$EXPORT_DIRPATH"/"$export_dirname"
+    rm -r "$export_dirpath"
+    mkdir -p "$export_dirpath"
+    
+    (cd godot-project && $GODOT_EXECUTABLE --export "$export_preset" --output "$export_dirpath"/"$executable_filename")
+
+    zip_filename="${export_dirname}_${version}.zip"
+    zip_filepath="${ZIP_DIRPATH}/${zip_filename}"
+    rm "$zip_filepath"
+    if [ "$export_preset" == "HTML5" ]; then
+        # itch.io expects index.html in root inside zip.
+        (cd "$EXPORT_DIRPATH/$export_dirname" && zip -r "$zip_filepath" *)
+    else
+        (cd "$EXPORT_DIRPATH" && zip -r "$zip_filepath" "$export_dirname")
+    fi
+}
+
+export_game "Linux/X11" "${name}_linux" "${name}_linux"
+export_game "Windows Desktop" "${name}_windows" "${name}_windows.exe"
+export_game "HTML5" "${name}_html" "index.html" # itch.io expects an index.html in zip
+
