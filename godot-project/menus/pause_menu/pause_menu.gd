2410cc40d3f91a1760eec8b5838879b6c5e6c592
Moisés J. Bonilla Caraballo <moisesjbc@gmail.com>
2019-01-24 17:30:31 +0000

    Added game over menu

--- godot-project/menus/pause_menu/pause_menu.gd
+++ godot-project/menus/pause_menu/pause_menu.gd
@@ -1,6 +1,9 @@
 extends CenterContainer
 
 
+func run():
+	pause_game()
+
 func pause_game():
 	get_tree().paused = true
 	visible = true
@@ -9,9 +12,10 @@ func unpause_game():
 	get_tree().paused = false
 	visible = false
 
-func _on_resume_button_pressed():
-	unpause_game()
-
 func _on_return_button_pressed():
 	unpause_game()
 	get_tree().change_scene("res://menus/main_menu/main_menu.tscn")
+
+func _on_restart_button_pressed():
+	unpause_game()
+	get_tree().change_scene("res://main/main.tscn")
