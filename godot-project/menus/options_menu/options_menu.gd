855c43d282a6c7aa86921be87e4c22e8b52b8ec7
Moisés J <moisesjbc@gmail.com>
2020-06-04 21:53:27 +0100

    music_system renamed as audio_system

--- godot-project/menus/options_menu/options_menu.gd
+++ godot-project/menus/options_menu/options_menu.gd
@@ -1,7 +1,7 @@
 extends Control
 
 var SceneUtils = preload("res://scripts/scene_utils.gd")
-const music_system_node_path = '/root/music_system'
+const audio_system_node_path = '/root/audio_system'
 
 func _ready():
 	print_sound_mode()
@@ -11,16 +11,16 @@ func _on_return_button_pressed():
 
 
 func _on_toggle_sound_button_pressed():
-	get_node(music_system_node_path).toggle_sound_mode()
+	get_node(audio_system_node_path).toggle_sound_mode()
 	print_sound_mode()
 
 
 func print_sound_mode():
-	var sound_mode = get_node(music_system_node_path).sound_mode
+	var sound_mode = get_node(audio_system_node_path).sound_mode
 	var sound_mode_label = ''
-	if sound_mode == get_node(music_system_node_path).SoundMode.FULL:
+	if sound_mode == get_node(audio_system_node_path).SoundMode.FULL:
 		sound_mode_label = 'FULL'
-	elif sound_mode == get_node(music_system_node_path).SoundMode.FX_ONLY:
+	elif sound_mode == get_node(audio_system_node_path).SoundMode.FX_ONLY:
 		sound_mode_label = 'FX ONLY'
 	else:
 		sound_mode_label = 'MUTED'
@@ -29,4 +29,4 @@ func print_sound_mode():
 
 
 func _on_volume_slider_value_changed(value):
-	get_node(music_system_node_path).set_volume(value)
+	get_node(audio_system_node_path).set_volume(value)
